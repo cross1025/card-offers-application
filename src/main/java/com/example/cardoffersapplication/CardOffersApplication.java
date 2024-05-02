38082bc43e6056a7afae0b9194b74987d633b312
Casey.Cross <ccrossaston@gmail.com>
2024-05-02 14:59:05 -0500

    Added needed child modules to parent application

---
+++ src/main/java/com/example/cardoffersapplication/CardOffersApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardoffersapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardOffersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardOffersApplication.class, args);
+    }
+
+}
